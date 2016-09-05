c4e90d947eeffb3086919f1fe75a0395f9eb3f84
Tran Dinh Trung <trandinhtrung176@gmail.com>
2016-09-05 18:57:47 +0000

    Add @kingcody's "SVG support feature"

--- config.mk
+++ config.mk
@@ -25,7 +25,7 @@ endif
 CPPFLAGS += -D_BSD_SOURCE -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} ${INIFLAGS}
 CFLAGS   += -g --std=gnu99 -pedantic -Wall -Wno-overlength-strings -Os ${STATIC} ${CPPFLAGS} ${EXTRACFLAGS}
 
-pkg_config_packs:="dbus-1 x11 freetype2 xext xft xscrnsaver glib-2.0 gio-2.0 pango cairo pangocairo"
+pkg_config_packs:="dbus-1 x11 freetype2 xext xft xscrnsaver glib-2.0 gio-2.0 pango cairo pangocairo librsvg-2.0"
 
 # check if we need libxdg-basedir
 ifeq (,$(findstring STATIC_CONFIG,$(CFLAGS)))
